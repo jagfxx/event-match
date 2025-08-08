2d7d6ea6b545fe349b1ecf828819682898ab271c
Jagf0511 <julianfernandez1103@gmail.com>
2025-08-08 17:59:33 -0500

    feat(config): add env variables support and improve server address logging

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,  // Hace que el módulo esté disponible en toda la app sin importar importarlo en otros módulos
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  const host = 'localhost';
+  await app.listen(port, host);
+  console.log(`Application is running on: http://${host}:${port}`);
 }
 bootstrap();
